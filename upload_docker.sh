433dc329aa451324aa8969366855374cf5f05964
Mustafa mahmoud <masterseen45@gmail.com>
2020-05-04 22:15:33 +0300

    Kubernetes and Intial Circle CI

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="mldevops"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker tag $dockerpath mustafamhasan/mldevops:latest
+
+docker push mustafamhasan/mldevops:latest
